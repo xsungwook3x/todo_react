3ce2cf2c58f4848a707b4cdc27f96d0a324950bd
Seonguk <xsungwook3x@naver.com>
2022-02-16 15:05:36 +0900

    아이템 리스트 렌더링

--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class Todo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={item:this.props.item};
+    }
+    
     render(){
         return(
             <div className='Todo'>
-                <input type="checkbox" id="todo0" name="todo0" value="todo0"></input>
-                <label for="todo0">Todo 컴포넌트 만들기</label>
+                <input type="checkbox" id={this.state.item.id} name={this.state.item.id} checked={this.state.item.done}></input>
+                <label id={this.state.item.id}> {this.state.item.title }</label>
             </div>
         );
     }
