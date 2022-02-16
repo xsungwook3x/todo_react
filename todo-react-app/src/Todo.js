8f47f3868114e8f28802cb22d4fe96494e7d80b7
Seonguk <xsungwook3x@naver.com>
2022-02-16 15:27:09 +0900

    material ui 사용

--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core"
 
 class Todo extends React.Component{
     constructor(props){
         super(props);
-        this.state={item:this.props.item};
+        this.state={item:props.item};
     }
     
     render(){
+        const item=this.state.item;
         return(
-            <div className='Todo'>
-                <input type="checkbox" id={this.state.item.id} name={this.state.item.id} checked={this.state.item.done}></input>
-                <label id={this.state.item.id}> {this.state.item.title }</label>
-            </div>
-        );
+        <ListItem>
+         <Checkbox checked={item.done} />
+        <ListItemText>
+          <InputBase
+            inputProps={{"aria-label":"naked"}}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+          />
+        </ListItemText>
+      </ListItem>
+    )
     }
 }
 
