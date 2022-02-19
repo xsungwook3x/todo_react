431430e023ad868511a5fac9d6faef9f0197de06
Seonguk <xsungwook3x@naver.com>
2022-02-19 22:15:38 +0900

    Service CORS

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,31 +1,77 @@
 import React from 'react';
 import Todo from './Todo';
-import {Paper,List} from "@material-ui/core";
+import AddTodo from './AddTodo';
+import {Paper,List, Container} from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      items:[
-        { id:"0",title:"Hello World 1", done: true},
-        { id:"1",title:"Hello World 2", done: false},
-      ], 
+      items:[ ], 
 
     };
   }
+
+  componentDidMount(){
+    const requestOptions ={
+      method: "GET",
+      headers:{"Content-Type":"application/json"},
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          this.setState({
+            items:response.data,
+          });
+        },
+        (error) => {
+          this.setState({error})
+        }
+      )
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id="ID-"+thisItems.length;
+    item.done=false;
+    thisItems.push(item);
+    this.setState({items:thisItems});
+    console.log("items :", this.state.items);
+  }
+
+  delete = (item) => {
+    const thisItems=this.state.items;
+    console.log("Before Update Items :", this.state.items)
+    const newItems=thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, () => {
+      console.log("Update Items : ", this.state.items)
+    })
+  }
+
   render() {
     var todoItems=this.state.items.length > 0 && (
       <Paper style={{margin:16}}>
         <List>
           {this.state.items.map((item,idx) => (
-            <Todo item={item} key={item.id}/>
+            <Todo item={item} key={item.id} delete={this.delete}/>
           ))}
         </List>
       </Paper>
     );
 
-    return <div className="App">{todoItems}</div>; 
+    return (
+    <div className="App">
+      <Container maxWidth="md">
+        <AddTodo add={this.add}/>
+        <div className='TodoList'>
+          {todoItems}
+        </div>
+      </Container>
+    </div>
+    ); 
     
   };
 }
